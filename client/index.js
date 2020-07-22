c104cf0217fed425585df772a25d2ca263c667fd
vyun-d1488 <ilyaspiypiy@gmail.com>
2020-07-23 01:33:35 +0600

    ne pomnyi che delal

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+("1a1b2f852436f0eb80b6d172c59bc98875bab13ba3863d22078cfec294f7cf1de29a23817026d6516546e");
+ReactDOM.render(<p>Hello world</p>, document.getElementById("root"));
--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 import express from "express";
-import request from "request";
-import cheerio from "cheerio";
 import "dotenv/config";
 
+const Nightmare = require("nightmare");
+const nightmare = Nightmare({
+      show: false,
+});
+
+nightmare
+      .goto("https://vk.com/eternalclassic")
+      .wait(2000)
+
+      .wait(2000)
+      .scrollTo(999999999999999999999999999, 0)
+      .wait(5000)
+      .evaluate(function () {
+            const page_post_thumb_wrap = document.getElementsByClassName(
+                  "page_post_thumb_wrap"
+            );
+            return document
+                  .getElementsByClassName("page_post_thumb_wrap")[0]
+                  .getAttribute("onclick");
+      })
+      .end()
+      .then(function (result) {
+            console.log(result);
+      });
 const PORT = process.env.PORT;
 const app = express();
 
